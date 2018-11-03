d92629e960b7f2ff936566d1610026bec5373790
Nate <npaulemon@gmail.com>
2018-11-03 12:47:10 -0400

    added message color and message object

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -1,25 +1,31 @@
 // objects here
 var Form = {
+    msgColor:"#000000",
+    generateMsgColor:function(){
+        this.msgColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    },
     validateForm: function (){
         var msg = document.forms["form"]["m"].value;
         document.getElementById("inputbox").value ="";
+        
         if (msg == "") {
             alert("Input must be filled out");
-        }else{
+        }
+        else{
             this.submitMessage(msg); 
         }
     },
-    recieveMessage:function (msg){
+    recieveMessage:function (msgObj){
         /*should recieve from server*/
-        document.getElementById("chatbox-container").innerHTML +="<br />"+mmsg;
-    },
-    recieveMessage :function(msg){
-        /*should recieve from server*/
-        document.getElementById("chatbox-container").innerHTML +="<br />"+msg;
-        
+        document.getElementById("chatbox-container").innerHTML += "<br/><span style='color:"+this.msgColor+"'>"+msgObj.msg+"</span>";
     },
     submitMessage:function(msg){
+        var msgObj = {
+            msg:msg,
+            msgColor:this.msgColor,
+            date:new Date()
+        };
         /*should go to server*/
-       this. recieveMessage(msg)
+       this. recieveMessage(msgObj)
     }
 }
--- public/javascript/eventHandler.js
+++ public/javascript/eventHandler.js
@@ -1,4 +1,7 @@
 window.addEventListener("load",function(){
+    // generate random msg color
+    Form.generateMsgColor();
+
     // page events here
     document.getElementById('submit-button').addEventListener("click",function(e){
         Form.validateForm();

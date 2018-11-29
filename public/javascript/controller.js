17a9019116feddeb4ea20e94f233dac17e547306
Nate <npaulemon@gmail.com>
2018-11-29 16:24:16 -0500

    added room id mechanism

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -9,6 +9,13 @@ var Form = {
         document.getElementById("inputbox").style.color = this.msgColor;
         document.getElementById("inputbox").style.border = "2px solid " + this.msgColor;
     },
+    generateRoomId:function() {
+        var S4 = function() {
+           return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        };
+        this.roomId = (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+        document.getElementById('input-roomid').value = this.roomId;
+    },
     validateForm: function (){
         var msg = document.forms["form"]["m"].value;
         var inputBox = document.getElementById("inputbox");
@@ -30,13 +37,16 @@ var Form = {
     },
     recieveMessage:function (msgObj){
         /*should recieve from server*/
-        document.getElementById("chatbox-container").innerHTML += "<br/><span style='color:#ccc'>"+moment(msgObj.date).format('MMMM Do YYYY, h:mm:ss a')+": </span> <span style='color:"+msgObj.msgColor+"'><strong>"+msgObj.msg+"</strong></span>";
+        if(msgObj.roomId == this.roomId){
+            document.getElementById("chatbox-container").innerHTML += "<br/><span style='color:#ccc'>"+moment(msgObj.date).format('MMMM Do YYYY, h:mm:ss a')+": </span> <span style='color:"+msgObj.msgColor+"'><strong>"+msgObj.msg+"</strong></span>";
+        }
     },
     submitMessage:function(msg){
         socket.emit('msgToServer',JSON.stringify({
             "msg":msg,
             "msgColor":this.msgColor,
-            "date":new Date()
+            "date":new Date(),
+            "roomId":this.roomId
         }))
 
     }
